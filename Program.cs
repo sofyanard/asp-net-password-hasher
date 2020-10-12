61d470aae18d147b5d07675634050f19e35b3640
Sofyan Ardianto <sofyanard@gmail.com>
2020-10-12 08:17:33 +0700

    code-example

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace passwordhash2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a password: ");
+            string password = Console.ReadLine();
+
+            CustomPasswordHasher customPasswordHasher = new CustomPasswordHasher();
+            string passwordHash = customPasswordHasher.HashPassword(password);
+
+            Console.WriteLine($"Hashed Password: {passwordHash}");
+            Console.ReadLine();
+
+            if (customPasswordHasher.VerifyPassword(passwordHash, password))
+            {
+                Console.WriteLine("Verification Success");
+            }
+            else
+            {
+                Console.WriteLine("Verification Failed");
+            }
+            Console.ReadLine();
+        }
+    }
+}
